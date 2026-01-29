943ff1cc1ad49530b598c9ebbf972acdf5ed9d1e
flippedpants <dakshrandomstuff@gmail.com>
2026-01-29 17:47:20 +0530

    made folder structure and setup the server

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Server is working.")
+})
+
+const connect_db = require("./config/db.js");
+connect_db();
+
+PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on Port - ${PORT}`)
+})
